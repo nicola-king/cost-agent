00f1f3f234d2a0d9248b278d0b2cf4a3d3300ba9
nicola-king <128955472+nicola-king@users.noreply.github.com>
2026-08-10 21:43:03 +0800

    chore: register migrated P04/P08 adapters

---
+++ releases/v0.3.0-rc1/source/app/plugins/__init__.py
@@ -0,0 +1,12 @@
+from . import (
+    p01_startup,
+    p02_boq,
+    p03_quantity,
+    p04_pricing,
+    p04_material_price,
+    p05_change,
+    p06_process,
+    p07_settlement,
+    p08_rules,
+    p08_quota,
+)
